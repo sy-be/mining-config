47a7c4ac51b416b607e58816fcb608fb7344f1b4
Sy <phoenix49@gmail.com>
2018-01-11 15:26:03 +0000

    updated rocm path

--- claymore-nanopool.bash
+++ claymore-nanopool.bash
@@ -7,5 +7,5 @@ export GPU_SINGLE_ALLOC_PERCENT=100
 
 HOSTNAME=`hostname`
 
-/home/<user>/apps/rocm-smi/rocm-smi --setsclk 3
+/home/<user>/apps/ROC-smi/rocm-smi --setsclk 3
 /home/<user>/apps/claymore/ethdcrminer64 -epool eth-eu1.nanopool.org:9999 -ewal <eth_address>.$HOSTNAME/<email> -epsw x -esm 0 -estale 0 -mode 1 -asm 0 -tstop 85 -tt 65 -dbg -1 -etha 2
